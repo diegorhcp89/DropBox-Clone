db6b7c79bdd7fc28d6db17e7690ef260c55a7821
Diego <diegoferreiradoamaral@gmail.com>
2021-06-16 21:40:34 -0300

    configurando evento de click

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -349,6 +349,7 @@ class DropBoxController {
             ${this.getFileIconView(file)}
             <div class="name text-center">${file.name}</div>
         `
+        this.initEventsLi(li);
 
         return li;
 
@@ -364,10 +365,64 @@ class DropBoxController {
 
                 let key = snapshotItem.key;
                 let data = snapshotItem.val();
-                
+
                 this.listFilesEl.appendChild(this.getFileView(data, key));
 
             });
+
+        });
+
+    }
+
+    initEventsLi(li){
+
+        li.addEventListener('click', e=>{
+
+            if (e.shiftKey) {
+
+                let firstLi = this.listFilesEl.querySelector('.selected');
+
+                if (firstLi) {
+
+                    let indexStart;
+                    let indexEnd;
+                    let lis = li.parentElement.childNodes;
+
+                    lis.forEach((el, index)=> {
+
+                        if (firstLi === el) indexStart = index;
+                        if (li === el) indexEnd = index;
+
+                    });
+
+                    let index = [indexStart, indexEnd].sort();
+
+                  
+                    lis.forEach((el, i)=> {
+
+                      if (i >= index[0] && i <=index[1]) {
+                          el.classList.add('selected');
+                      }
+
+                    });
+
+                    return true;
+
+                }
+
+            }
+
+            if (!e.ctrlKey) {
+
+                this.listFilesEl.querySelectorAll('li.selected').forEach(el=>{
+
+                    el.classList.remove('selected');
+
+                });
+
+            }
+
+            li.classList.toggle('selected');
 
         });
 
