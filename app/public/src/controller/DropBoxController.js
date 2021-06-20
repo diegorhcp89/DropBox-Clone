552c5900cbaf1f9ff1bae4bdf48d2b73006b3234
Diego <diegoferreiradoamaral@gmail.com>
2021-06-20 18:36:40 -0300

    Configurando para excluir arquivos e pastas do diretorio Firebase

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -691,10 +691,7 @@ class DropBoxController {
                     break;
 
                 default:
-                    window.open('/file?path=' + file.path);
-
-
-
+                    window.open(file.path);
             }
 
         });
