7d28accfd6f6af0bb6abeb28bc5114af49d8925f
Diego <diegoferreiradoamaral@gmail.com>
2021-06-20 18:31:23 -0300

    Configurando para excluir arquivos e pastas do diretorio Firebase

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -49,6 +49,62 @@ class DropBoxController {
 
     }
 
+    removeFolderTask(ref, name){
+
+        return new Promise((resolve, reject) => {
+
+            let folderRef = this.getFirebaseRef(ref + '/' + name);
+
+            folderRef.on('value', snapshot => {
+
+                folderRef.off('value');
+
+                snapshot.forEach(item=>{
+
+                    let data = item.val();
+                    data.key = item.key;
+
+                    if (data.type === 'folder') {
+
+                        this.removeFolderTask(ref + '/' + name, data.name).then(()=>{
+
+                            resolve({
+                                fields:{
+                                    key: data.key
+                                }
+                            });
+
+                        }).catch(err=>{
+                            reject(err);
+                        });
+
+                    } else if(data.type) {
+
+                        this.removeFile(ref + '/' + name, data.name).then(()=>{
+
+                            resolve({
+                                fields:{
+                                    key: data.key
+                                }
+                            });
+
+                        }).catch(err=>{
+                            reject(err);
+                        });
+
+                    }
+
+                });
+
+                folderRef.remove();
+                
+
+            });
+
+        });
+
+    }
+
     removeTask(){
 
         let promises =[];
@@ -58,12 +114,35 @@ class DropBoxController {
             let file = JSON.parse(li.dataset.file);
             let key = li.dataset.key;
 
-            let formData = new FormData();
+            promises.push(new Promise((resolve, reject)=>{
 
-            formData.append('path', file.path);
-            formData.append('key', key);
+                if (file.type === 'folder') {
 
-            promises.push(this.ajax('/file', 'DELETE', formData));
+                    this.removeFolderTask(this.currentFolder.join('/'), file.name).then(() => {
+
+                        resolve({
+                            fields:{
+                                key
+                            }
+                        });
+
+                    });
+
+                } else if (file.type) {
+
+                    this.removeFile(this.currentFolder.join('/'), file.name).then(()=>{
+
+                        resolve({
+                            fields:{
+                                key
+                            }
+                        });
+    
+                    });
+
+                }
+
+            }));
 
         });
 
@@ -71,27 +150,43 @@ class DropBoxController {
 
     }
 
+    removeFile(ref, name){
+
+        let fileRef = firebase.storage().ref(ref).child(name);
+
+        return fileRef.delete();
+    }
+
     initEvents(){
 
-        this.btnDelete.addEventListener('click', e=>{
+        this.btnNewFolder.addEventListener('click',event=>{
 
-            this.removeTask().then(responses=>{
+			let name = prompt("Digite o nome da nova pasta:");
 
-                responses.forEach(response=>{
+			if(name){
+				this.getFirebaseRef().push().set({
+					name,
+					type:'folder',
+					path: this.currentFolder.join('/')
+				});
+			}
 
-                    if (response.fields.key) {
+		});
 
-                        this.getFirebaseRef().child
-                        (response.fields.key).remove();
+		this.btnDelete.addEventListener('click',event=>{
 
-                    }
+			this.removeTask().then(responses=>{
+				responses.forEach(response=>{
 
-                })
+					if(response.fields.key){
 
-            }).catch(err=>{
-                console.error(err);
+						this.getFirebaseRef().child(response.fields.key).remove();
 
-            });
+					}
+
+				})
+			});
+
 
         });
 
