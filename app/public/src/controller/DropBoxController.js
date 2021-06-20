eb0f9f994f04e0c24d57ab04c6c0b9a641586058
Diego <diegoferreiradoamaral@gmail.com>
2021-06-20 00:32:04 -0300

    Configurando para criação de pastas e rotas

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -2,6 +2,11 @@ class DropBoxController {
 
     constructor() {
 
+        this.currentFolder = ['DIEGOBOX'];
+
+        this.onselectionchange = new Event('selectionchange');
+
+        this.navEl = document.querySelector('#browse-location');
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector('#react-snackbar-root');
@@ -16,7 +21,8 @@ class DropBoxController {
 
         this.connectFirebase();
         this.initEvents();
-        this.readFiles();
+
+        this.openFolder();
 
     }
 
@@ -66,6 +72,22 @@ class DropBoxController {
     }
 
     initEvents(){
+
+        this.btnNewFolder.addEventListener('click', e=>{
+
+            let name = prompt('Nome da nova pasta:');
+
+            if (name) {
+
+                this.getFirebaseRef().push().set({
+                    name,
+                    type:'folder',
+                    path:this.currentFolder.join('/')
+                })
+
+            }
+
+        });
 
         this.btnDelete.addEventListener('click', e=>{
 
@@ -168,9 +190,11 @@ class DropBoxController {
         this.btnSendFileEl.disable = false;
     }
 
-    getFirebaseRef(){
+    getFirebaseRef(path){
 
-        return firebase.database().ref('files');
+        if (!path) path = this.currentFolder.join('/');
+
+        return firebase.database().ref(path);
 
     }
 
@@ -190,7 +214,7 @@ class DropBoxController {
 
                 ajax.onload = event => {
 
-                    this.modalShow(false);
+                    // this.modalShow(false);
 
                     try {
                         resolve(JSON.parse(ajax.responseText));
@@ -462,6 +486,8 @@ class DropBoxController {
 
     readFiles(){
 
+        this.lastFolder = this.currentFolder.join('/');
+
         this.getFirebaseRef().on('value', snapshot => {
 
             this.listFilesEl.innerHTML = '';
@@ -471,7 +497,11 @@ class DropBoxController {
                 let key = snapshotItem.key;
                 let data = snapshotItem.val();
 
-                this.listFilesEl.appendChild(this.getFileView(data, key));
+                if (data.type) {
+
+                    this.listFilesEl.appendChild(this.getFileView(data, key));
+
+                }
 
             });
 
@@ -479,11 +509,92 @@ class DropBoxController {
 
     }
 
+    openFolder(){
+
+        if (this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
+
+        this.renderNav();
+        this.readFiles();
+
+    }
+
+    renderNav(){
+
+        let nav = document.createElement('nav');
+        let path = [];
+
+        for (let i = 0; i < this.currentFolder.length; i++) {
+
+            let folderName = this.currentFolder[i];
+            let span = document.createElement('span');
+
+            path.push(folderName);
+
+            if ((i + 1) === this.currentFolder.length) {
+
+                span.innerHTML = folderName;
+
+            } else {
+
+                span.className = 'breadcrumb-segment__wrapper';
+                span.innerHTML = `
+                    <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                        <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
+                    </span>
+                    <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                        <title>arrow-right</title>
+                        <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+                    </svg>
+                `;
+
+            }
+
+            nav.appendChild(span);
+
+        }
+
+        this.navEl.innerHTML = nav.innerHTML;
+
+        this.navEl.querySelectorAll('a').forEach(a=>{
+
+            a.addEventListener('click', e=>{
+
+                e.preventDefault();
+
+                this.currentFolder = a.dataset.path.split('/');
+
+                this.openFolder();
+
+            });
+
+        });
+
+
+    }
+
     initEventsLi(li){
 
-        li.addEventListener('click', e=>{
+        li.addEventListener('dblclick', e=>{
 
-            this.onselectionchange = new Event('selectionchange');
+            let file = JSON.parse(li.dataset.file);
+
+            switch (file. type) {
+
+                case 'folder':
+                    this.currentFolder.push(file.name);
+                    this.openFolder();
+                    break;
+
+                default:
+                    window.open('/file?path=' + file.path);
+
+
+
+            }
+
+        });
+
+        li.addEventListener('click', e=>{
 
             if (e.shiftKey) {
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/file',(req, res)=>{
+
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)) {
+
+    fs.readFile(path, (err, data)=>{
+
+      if (err) {
+
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+
+      } else {
+
+        res.status(200).end(data);
+
+      }
+
+    });
+
+  } else {
+
+    res.status(404).json({
+      error: 'File not found.'
+    });
+
+  }
+
+});
+
 router.delete('/file', (req, res)=>{
 
   let form = new formidable.IncomingForm({
@@ -19,7 +52,7 @@ router.delete('/file', (req, res)=>{
 
     let path = "./" + fields.path;
 
-    if (fs.existsSync(fields.path)) {
+    if (fs.existsSync(path)) {
 
       fs.unlink(path, err=>{
 
@@ -39,6 +72,12 @@ router.delete('/file', (req, res)=>{
 
       });
 
+    } else {
+
+      res.status(404).json({
+        error: 'File not found.'
+      });
+  
     }
 
   });
